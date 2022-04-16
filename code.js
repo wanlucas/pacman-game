3e28b04e6e40f09a2c574dd12a0b882c192e7044
wan lucas <wan.lucas.90@gmail.com>
2022-04-16 17:47:30 -0300

    add inpus and player

--- code.js
+++ code.js
@@ -15,23 +15,92 @@ class Boundarie {
     this.width = 60, this.height = 60;
   }
   draw(){
+    c.fillStyle = 'black';
     c.fillRect(
       this.position.x, this.position.y,
       this.width, this.height
       );
   };
+  update(){
+  
+  }
 };
 
-const map = [
-  ['+','+','+','+','+','+'],
-  ['+','-','-','-','+','+'], 
-  ['+','-','-','-','-','+'],
-  ['+','-','-','+','+','+'],
-  ['+','+','+','+','+','+']
-];
+class Player {
+  constructor(position) {
+    this.position = {
+      x: position.x,
+      y: position.y
+    }
+    this.velocity = {
+      x:0,
+      y:0
+    }
+    this.r = (Boundarie.width / 2) - 5
+  }
+
+  draw() {
+    c.beginPath();
+    c.fillStyle = 'yellow';
+    c.arc(
+      this.position.x, this.position.y,
+      this.r, 0, Math.PI * 2
+    );
+    c.fill();
+    c.closePath();
+  }
+
+  updateInputs() {
+    if(keys.right.pressed && keys.lastKey === 'd')
+      this.velocity.x = 5;
+    else if(keys.left.pressed && keys.lastKey === 'a') 
+      this.velocity.x = -5;
+    else if(keys.bottom.pressed && keys.lastKey === 's')
+      this.velocity.y = 5;
+    else if(keys.top.pressed && keys.lastKey === 'w') 
+      this.velocity.y = -5;
+  }
+
+  update() {
+    this.draw();
+    this.updateInputs();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
 const boundaries = [];
 
-const createMap = () => {
+const player = new Player(
+  position = {
+    x: Boundarie.width + Boundarie.width / 2,
+    y: Boundarie.height + Boundarie.height / 2
+  }
+);
+
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  },
+  bottom: {
+    pressed: false
+  },
+  top: {
+    pressed: false
+  },
+  lastKey: null
+};
+
+function createMap() {
+  const map = [
+    ['+','+','+','+','+','+'],
+    ['+','-','-','-','-','+'], 
+    ['+','-','+','+','-','+'],
+    ['+','-','-','-','-','+'],
+    ['+','+','+','+','+','+'],
+  ];
 
   map.forEach((row,i) => {
     row.forEach((block, i2) => {
@@ -49,11 +118,31 @@ const createMap = () => {
   });
 };
 
-const run = () => {
+function run() {
+  c.clearRect(0,0,canvas.width, canvas.height);
   boundaries.forEach((boundarie) => boundarie.draw());
+  player.update();
+  requestAnimationFrame(run);
 }
+
+function addInputEvents() {
+  addEventListener('keydown', ({ key }) => {
+    keys.lastKey = key;
+    if(key === 'd') keys.right.pressed = true;
+    else if(key === 'a') keys.left.pressed = true;
+    else if(key === 'w') keys.top.pressed = true;
+    else if(key === 's') keys.bottom.pressed = true;
+  });
+  addEventListener('keyUp', ({ key }) => {
+    if(key === 'd') keys.right.pressed = false;
+    else if(key === 'a') keys.left.pressed = false;
+    else if(key === 'w') keys.top.pressed = false;
+    else if(key === 's') keys.bottom.pressed = false;
+  });
+};
 
 addEventListener('load', ()=> {
   createMap();
+  addInputEvents();
   run();
 })
